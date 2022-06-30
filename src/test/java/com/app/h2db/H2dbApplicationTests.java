31af9a83c58f0ae41e9fffa65dbc512d237b31e1
vinaymcu <vinaymcu@gmail.com>
2022-06-30 09:20:16 +0530

    addedd alll

---
+++ src/test/java/com/app/h2db/H2dbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.h2db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2dbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
